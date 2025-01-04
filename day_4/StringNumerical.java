0cb1195695c9e25a7ef997e99c30655c7b920bca
MeghanaGuntaka <meghanareddy.guntaka343@gmail.com>
2025-01-04 22:03:10 +0530

    day 4

--- day_4/StringNumerical.java
+++ day_4/StringNumerical.java
@@ -1,5 +1,23 @@
 package day_4;
 
 public class StringNumerical {
+	public static void main(String[] args) {
+		String s = "A1B2C3D4";
+        String emp= "";
+
+        for (int i = 0; i<s.length(); i += 2) {
+            char letter=s.charAt(i);
+            int count = Character.getNumericValue(s.charAt(i + 1));
+
+            for (int j = 0; j < count; j++) {
+                emp+= letter;
+            }
+        }
+
+        System.out.println(emp);
+  
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- day_4/WordsReverse.java
+++ day_4/WordsReverse.java
@@ -1,5 +1,21 @@
 package day_4;
 
 public class WordsReverse {
+	static char[] c;
+	static String empty="";
+	static String str="Meghana";
+	public static void main(String[] a) {
+		for(int i=str.length()-1;i>=0;i--) {
+			empty+=str.charAt(i);
+//			empty=c;
+			//c=str.toCharArray();
+			System.out.print(str.charAt(i));
+			
+		}
+		
+		//System.out.println(empty);
+		
+	}
+
 
 }
