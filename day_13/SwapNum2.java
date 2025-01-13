594ea7a0ec31f1ae8432b4aaf937344af816de32
MeghanaGuntaka <meghanareddy.guntaka343@gmail.com>
2025-01-13 21:15:11 +0530

    day 13

---
+++ day_13/SwapNum2.java
@@ -0,0 +1,14 @@
+package day_13;
+
+public class SwapNum2 {
+	public static void main(String[] args) {
+		int i=10;
+		int j=20;
+		i=i + j;
+		j=i-j;
+		i=i-j;
+		System.out.println("i="+i);
+		System.out.println("j="+j);
+	}
+
+}
