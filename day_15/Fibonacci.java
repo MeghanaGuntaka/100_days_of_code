e2ec389c2b07e76dbb3038ee1887a5d59739d03c
MeghanaGuntaka <meghanareddy.guntaka343@gmail.com>
2025-01-15 21:44:32 +0530

    day 15

---
+++ day_15/Fibonacci.java
@@ -0,0 +1,21 @@
+package day_15;
+
+public class Fibonacci {
+	static int fib(int n)
+	{
+		if(n==0)
+			return 0;
+		if(n==1)
+			return 1;
+		return fib(n-1)+fib(n-2);
+	}
+	public static void main(String[] args)
+	{
+		java.util.Scanner sc=new java.util.Scanner(System.in);
+		System.out.println("Enter the number");
+		int m=sc.nextInt();
+		int f=fib(m);
+		System.out.println(f);
+	}
+
+}
